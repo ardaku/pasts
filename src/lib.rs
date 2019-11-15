ceeacbbbe36d31a04d743a0efb3294c40270028e
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-11-15 11:41:06 -0600

    Borrow executor and waker from executor and woke.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,97 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+extern crate alloc;
+
+use {
+    alloc::{boxed::Box, sync::Arc},
+    core::{
+        future::Future,
+        pin::Pin,
+        task::{Context, Poll},
+    },
+    spin::Mutex,
+    woke::{waker_ref, Woke},
+};
+
+use smallvec::*;
+
+/// Executor holds a list of futures to be polled.
+pub struct Executor {
+    /// Tasks as a smallvec to try to keep it in the stack for as long as
+    /// possible
+    tasks: SmallVec<[Arc<Task>; 64]>,
+}
+
+impl Default for Executor {
+    fn default() -> Self {
+        Executor {
+            tasks: SmallVec::new(),
+        }
     }
+}
+
+/// Task is our unit of execution and holds a future are waiting on
+struct Task {
+    pub future: Mutex<Pin<Box<dyn Future<Output = ()> + Send + 'static>>>,
+}
+
+/// Implement what we would like to do when a task gets woken up
+impl Woke for Task {
+    fn wake_by_ref(_: &Arc<Self>) {
+        // poll everything because future is done and may have created
+        // conditions for something to finish
+        Executor::run()
+    }
+}
+
+impl Executor {
+    // Add a task on the global executor
+    fn spawn(future: impl Future<Output = ()> + 'static + Send) {
+        let mut e = globals::get::<Executor>();
+        e.add_task(future);
+        e.poll_tasks();
+    }
+
+    // Poll all tasks on global executor
+    fn run() {
+        let mut e = globals::get::<Executor>();
+        Executor::poll_tasks(&mut e);
+    }
+
+    /// Add task for a future to the list of tasks
+    fn add_task(&mut self, future: impl Future<Output = ()> + 'static + Send) {
+        // store our task
+        let task = Arc::new(Task {
+            future: Mutex::new(Box::pin(future)),
+        });
+        self.tasks.push(task);
+    }
+
+    /// For every current task, iterate through each one and poll them, if they
+    /// are not done add them to end of list. If they are done, remove them from
+    /// list of tasks.
+    fn poll_tasks(&mut self) {
+        let count = self.tasks.len();
+        for _ in 0..count {
+            let task = self.tasks.remove(0);
+            let mut is_pending = false;
+            {
+                let mut future = task.future.lock();
+                // make a waker for our task
+                let waker = waker_ref(&task);
+                // poll our future and give it a waker
+                let context = &mut Context::from_waker(&*waker);
+                if let Poll::Pending = future.as_mut().poll(context) {
+                    is_pending = true;
+                }
+            }
+            if is_pending {
+                self.tasks.push(task);
+            }
+        }
+    }
+}
+
+/// Give a future to the global executor to be polled.
+pub fn spawn(future: impl Future<Output = ()> + 'static + Send) {
+    Executor::spawn(future);
 }
---
+++ src/waker.rs
@@ -0,0 +1,102 @@
+use core::marker::PhantomData;
+use core::mem;
+use core::mem::ManuallyDrop;
+use core::ops::Deref;
+use core::task::{RawWaker, RawWakerVTable, Waker};
+use alloc::sync::Arc;
+
+pub trait Woke: Send + Sync {
+    fn wake(self: Arc<Self>) {
+        Self::wake_by_ref(&self)
+    }
+
+    fn wake_by_ref(arc_self: &Arc<Self>);
+}
+
+pub fn waker_vtable<W: Woke>() -> &'static RawWakerVTable {
+    &RawWakerVTable::new(
+        clone_arc_raw::<W>,
+        wake_arc_raw::<W>,
+        wake_by_ref_arc_raw::<W>,
+        drop_arc_raw::<W>,
+    )
+}
+
+pub fn waker<W>(wake: Arc<W>) -> Waker
+where
+    W: Woke,
+{
+    let ptr = Arc::into_raw(wake) as *const ();
+
+    unsafe { Waker::from_raw(RawWaker::new(ptr, waker_vtable::<W>())) }
+}
+
+unsafe fn increase_refcount<T: Woke>(data: *const ()) {
+    let arc = mem::ManuallyDrop::new(Arc::<T>::from_raw(data as *const T));
+    let _arc_clone: mem::ManuallyDrop<_> = arc.clone();
+}
+
+unsafe fn clone_arc_raw<T: Woke>(data: *const ()) -> RawWaker {
+    increase_refcount::<T>(data);
+    RawWaker::new(data, waker_vtable::<T>())
+}
+
+unsafe fn wake_arc_raw<T: Woke>(data: *const ()) {
+    let arc: Arc<T> = Arc::from_raw(data as *const T);
+    Woke::wake(arc);
+}
+
+unsafe fn wake_by_ref_arc_raw<T: Woke>(data: *const ()) {
+    // Retain Arc, but don't touch refcount by wrapping in ManuallyDrop
+    let arc = mem::ManuallyDrop::new(Arc::<T>::from_raw(data as *const T));
+    Woke::wake_by_ref(&arc);
+}
+
+unsafe fn drop_arc_raw<T: Woke>(data: *const ()) {
+    drop(Arc::<T>::from_raw(data as *const T))
+}
+
+#[derive(Debug)]
+pub struct WakerRef<'a> {
+    waker: ManuallyDrop<Waker>,
+    _marker: PhantomData<&'a ()>,
+}
+
+impl<'a> WakerRef<'a> {
+    pub fn new(waker: &'a Waker) -> Self {
+        let waker = ManuallyDrop::new(unsafe { core::ptr::read(waker) });
+        WakerRef {
+            waker,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn new_unowned(waker: ManuallyDrop<Waker>) -> Self {
+        WakerRef {
+            waker,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl Deref for WakerRef<'_> {
+    type Target = Waker;
+
+    fn deref(&self) -> &Waker {
+        &self.waker
+    }
+}
+
+#[inline]
+pub fn waker_ref<W>(wake: &Arc<W>) -> WakerRef<'_>
+where
+    W: Woke,
+{
+    let ptr = (&**wake as *const W) as *const ();
+
+    let waker =
+        ManuallyDrop::new(unsafe {
+            Waker::from_raw(RawWaker::new(ptr, waker_vtable::<W>()))
+        });
+    WakerRef::new_unowned(waker)
+}
