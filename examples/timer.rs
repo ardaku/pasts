25c4bc42f0959d05e05b154bc92b369e27c73573
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-11-22 23:10:12 -0600

    Write new executor code.

--- examples/timer.rs
+++ examples/timer.rs
@@ -55,7 +55,14 @@ struct SharedState {
     waker: Option<Waker>,
 }
 
+// Example executor, since const generics aren't stable yet - create with macro
+// passing in max_capacity.
 struct Executor {
+    // List of futures.
+    // futures: [impl Future<Output = ()> + 'static; 1],
+    // 
+
+    // Old thing
     ready_queue: Receiver<Arc<Task>>,
 }
 
@@ -99,26 +106,109 @@ impl Woke for Task {
 
 impl Executor {
     fn run(&self) {
-        while let Ok(task) = self.ready_queue.recv() {
+        println!("Starting executor!");
+        while let Ok(task) = self.ready_queue.recv() { // Blocking call
+            println!("Executor Loop!");
             let mut future_slot = task.future.lock().unwrap();
+            println!("Looking for future…");
             if let Some(mut future) = future_slot.take() {
                 let waker = waker_ref(&task);
+                println!("Got Waker");
                 let context = &mut Context::from_waker(&*waker);
+                println!("Got Context");
                 if let Poll::Pending = future.as_mut().poll(context) {
                     *future_slot = Some(future);
                 }
+                println!("Poll Finished");
             }
         }
     }
 }
 
+/// Pins a value on the stack.
+///
+/// ```
+/// # use pin_utils::pin_mut;
+/// # use core::pin::Pin;
+/// # struct Foo {}
+/// let foo = Foo { /* ... */ };
+/// pin_mut!(foo);
+/// let _: Pin<&mut Foo> = foo;
+/// ```
+#[macro_export]
+macro_rules! pin_mut {
+    ($($x:ident),* $(,)?) => { $(
+        // Move the value to ensure that it is owned
+        let mut $x = $x;
+        // Shadow the original binding so that it can't be directly accessed
+        // ever again.
+        #[allow(unused_mut)]
+        let mut $x = unsafe {
+            std::pin::Pin::new_unchecked(&mut $x)
+        };
+    )* }
+}
+
 fn main() {
-    let (executor, spawner) = new_executor_and_spawner();
+    // TODO: Eventually generate executor (this function) with macro.
+    use std::sync::atomic::{AtomicBool, Ordering};
+
+    static mut FUTURE_ONE_CONDVAR: AtomicBool = AtomicBool::new(true);
+
+    pub struct FutureOneTask();
+
+    impl Woke for FutureOneTask {
+        fn wake_by_ref(_arc_self: &Arc<Self>) {
+            unsafe {
+                FUTURE_ONE_CONDVAR.store(true, Ordering::Relaxed);
+            }
+
+            /*let cloned = arc_self.clone();
+            arc_self
+                .task_sender
+                .send(cloned)
+                .expect("too many tasks queued");*/
+        }
+    }
+
+    let task = Arc::new(FutureOneTask());
+    let mut future_one = async {
+        println!("Waiting 2 seconds…");
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Done!");
+    };
+    pin_mut!(future_one);
+
+    // Check for any futures that are ready
+
+    loop {
+        if unsafe { FUTURE_ONE_CONDVAR.load(Ordering::Relaxed) } {
+            // This runs whenever woke.
+            let waker = waker_ref(&task);
+            println!("Got Waker");
+            let context = &mut Context::from_waker(&*waker);
+            println!("Got Context");
+            if let Poll::Pending = future_one.as_mut().poll(context) {
+                // Go back to "sleep".
+                unsafe {
+                    FUTURE_ONE_CONDVAR.store(false, Ordering::Relaxed);
+                }
+            } else {
+                println!("Complete!");
+                break;
+            }
+            println!("Poll Finished");
+        }
+    }
+
+    // // // 
+
+/*    let (executor, spawner) = new_executor_and_spawner();
     spawner.spawn(async {
         println!("howdy!");
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("done!");
     });
     drop(spawner);
-    executor.run();
+    executor.run();*/
 }
