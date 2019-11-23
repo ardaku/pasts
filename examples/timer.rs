4999d187484d3373b716f841388185b8f844c136
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-11-23 11:34:25 -0600

    Add block_on() function.

--- examples/timer.rs
+++ examples/timer.rs
@@ -1,51 +1,13 @@
-use pasts::Wake;
-
-mod futures;
-
-use crate::futures::TimerFuture;
+mod timerfuture;
 
 fn main() {
-    // TODO: Eventually generate executor (this function) with macro.
-    use std::{
-        sync::atomic::{AtomicBool, Ordering},
-        task::{Context, Poll},
-        future::Future,
-    };
-
-    static mut FUTURE_CONDVARS: [AtomicBool; 1] = [AtomicBool::new(true)];
-
-    pub struct FutureZeroTask();
-
-    impl Wake for FutureZeroTask {
-        unsafe fn wake_up() {
-            FUTURE_CONDVARS[0].store(true, Ordering::Relaxed);
-        }
-    }
-
-    pasts::let_pin! {
-        future_one = async {
+    let ret = pasts::block_on(
+        async {
             println!("Waiting 2 seconds…");
-            TimerFuture::new(std::time::Duration::new(2, 0)).await;
-            println!("Done!");
-        };
-        task = FutureZeroTask();
-    };
-
-    // Check for any futures that are ready
-
-    loop {
-        if unsafe { FUTURE_CONDVARS[0].load(Ordering::Relaxed) } {
-            // This runs whenever woke.
-            let task = unsafe { FutureZeroTask::into_waker(&*task) };
-            let context = &mut Context::from_waker(&task);
-            if let Poll::Pending = future_one.as_mut().poll(context) {
-                // Go back to "sleep".
-                unsafe {
-                    FUTURE_CONDVARS[0].store(false, Ordering::Relaxed);
-                }
-            } else {
-                break;
-            }
+            timerfuture::TimerFuture::new(std::time::Duration::new(2, 0)).await;
+            println!("Waited 2 seconds.");
+            "Complete!"
         }
-    }
+    );
+    println!("Future returned: \"{}\"", ret);
 }
