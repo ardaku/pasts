f4071b2bb4fd9dbb0729ab67ae0f7557eba869f1
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-12-01 11:27:09 -0600

    Add `Moved` to Task.

--- examples/multi.rs
+++ examples/multi.rs
@@ -1,35 +1,44 @@
 mod timerfuture;
 
-async fn timer(how_long: u64) -> u64 {
-    timerfuture::TimerFuture::new(std::time::Duration::new(how_long, 0)).await;
-    how_long
+#[derive(Debug)]
+struct Length(u64);
+
+impl Drop for Length {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0);
+    }
 }
 
-/*
+async fn timer(how_long: u64) -> Length {
+    timerfuture::TimerFuture::new(std::time::Duration::new(how_long, 0)).await;
+    Length(how_long)
+}
+
 macro_rules! join {
     ($($future:ident),* $(,)?) => {
         {
             use pasts::{
                 let_pin,
                 _pasts_hide::stn::mem::MaybeUninit,
+                Task::Wait,
             };
 
             let_pin! {
                 $(
-                    $future = Some($future);
-                )
-            }
+                    $future = Wait($future);
+                )*
+            };
         }
     };
-}*/
+}
 
 fn main() {
-    pasts::let_pin! {
-        one = pasts::Task::Wait(timer(1));
-        two = pasts::Task::Wait(timer(2));
-    };
-
     let ret = pasts::block_on(async {
+        pasts::let_pin! {
+            one = pasts::Task::Wait(timer(1));
+            two = pasts::Task::Wait(timer(2));
+        };
+
         for _ in 0..2 { // Push 2 futures to completion.
             pasts::select! {
                 a = one => println!("Finished 1"),
@@ -38,10 +47,8 @@ fn main() {
         }
 
         (
-            one.unwrap(),
-            two.unwrap(),
-//            pasts::_pasts_hide::assume_init(one),
-//            pasts::_pasts_hide::assume_init(two),
+            one.take().unwrap(),
+            two.take().unwrap(),
         )
     });
     println!("Future returned: \"{:?}\"", ret);
--- src/task.rs
+++ src/task.rs
@@ -2,6 +2,7 @@ use crate::_pasts_hide::{
     stn::{
         future::Future,
         pin::Pin,
+        mem::replace,
     }
 };
 
@@ -13,6 +14,8 @@ pub enum Task<F, O>
     Wait(F),
     /// Future has completed.
     Done(O),
+    /// Future output has been moved.
+    Moved,
 }
 
 impl<F, O> Task<F, O>
@@ -23,7 +26,7 @@ impl<F, O> Task<F, O>
     pub fn is_wait(&self) -> bool {
         match self {
             Self::Wait(_) => true,
-            Self::Done(_) => false,
+            _ => false,
         }
     }
 
@@ -33,24 +36,38 @@ impl<F, O> Task<F, O>
         !self.is_wait()
     }
 
+    #[inline(always)]
     pub fn as_mut(&mut self) -> Option<&mut F> {
         match *self {
             Task::Wait(ref mut x) => Some(x),
-            Task::Done(_) => None,
+            _ => None,
         }
     }
 
     #[allow(unsafe_code)]
+    #[inline(always)]
     pub fn as_pin_mut(self: Pin<&mut Self>) -> Option<Pin<&mut F>> {
         unsafe {
             Pin::get_unchecked_mut(self).as_mut().map(|x| Pin::new_unchecked(x))
         }
     }
 
-    pub fn unwrap(&self) -> &O {
+    /// Replace self with `Task::Moved` and return what it was.
+    #[allow(unsafe_code)]
+    #[inline(always)]
+    pub fn take(self: Pin<&mut Self>) -> Self {
+        unsafe {
+            replace(Pin::get_unchecked_mut(self), Task::Moved)
+        }
+    }
+
+    /// Get 
+    #[inline(always)]
+    pub fn unwrap(self) -> O {
         match self {
-            Task::Done(output) => &output,
+            Task::Done(output) => output,
             Task::Wait(_) => panic!("unwrap() called on an incomplete task!"),
+            Task::Moved => panic!("unwrap() called on a moved task!"),
         }
     }
 }
